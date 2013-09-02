b512158a70ff71bf78bec711a0b95132e7c4bc7c
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-09-02 18:47:36 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='storm',
+    version='0.1-dev',
+    url='https://github.com/thiderman/storm',
+    author='Lowe Thiderman, Niclas Helbro',
+    author_email='lowe.thiderman@gmail.com, niclas.helbro@gmail.com',
+    description=('inotify-based dzen2 runner'),
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'storm = storm:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+    ],
+)
